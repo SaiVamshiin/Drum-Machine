eb3f80e3c72ab219288327dab561112e07932d94
B Sai Vamshi <134629654+SaiVamshiin@users.noreply.github.com>
2023-08-26 22:07:16 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import * as React from "https://cdn.skypack.dev/react@17.0.1";
 import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 
-const firstSoundsGroup = [
+const firstSoundsGroup =[
 {
   keyCode: 81,
   key: 'Q',
